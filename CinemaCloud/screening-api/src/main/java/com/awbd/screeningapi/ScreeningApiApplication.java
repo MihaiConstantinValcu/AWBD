6af40a5a6adcf62ce0426c53a2881eb1df5ba75c
Mihai Valcu <mihai.c.valcu@gmail.com>
2025-06-04 14:14:32 +0300

    Started migrating Cinema app to Spring Cloud

---
+++ CinemaCloud/screening-api/src/main/java/com/awbd/screeningapi/ScreeningApiApplication.java
@@ -0,0 +1,13 @@
+package com.awbd.screeningapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreeningApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreeningApiApplication.class, args);
+    }
+
+}
---
+++ CinemaCloud/screening-api/src/test/java/com/awbd/screeningapi/ScreeningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awbd.screeningapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreeningApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
